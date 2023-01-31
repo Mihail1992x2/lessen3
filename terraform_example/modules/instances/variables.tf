11fa020e51fd2853064313e1eac7791c0a52ee57
Mihail1992x2 <119675734+Mihail1992x2@users.noreply.github.com>
2023-01-31 15:15:28 +0200

    Add files via upload

---
+++ terraform_example/modules/instances/variables.tf
@@ -0,0 +1,21 @@
+ variable "web_instance_type" {
+    default = "t2.micro"
+  
+}
+
+
+variable "instance_name" {
+    default = "MyPC"
+  
+}
+
+variable "vpc_1_id" {}
+variable "aws_subnet_a_id" {}
+variable "aws_security_group_sg_web_id" {}
+variable "aws_eip_instance1_id" {}
+variable "web_1_instance_id" {}
+#variable "launch_config_id" {
+  
+#}
+#variable "aws_route_table_def_route_1" {}
+#variable "aws_route_table_association_a" {  }
