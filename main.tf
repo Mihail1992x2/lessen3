5786e0cee961379c88571a341e70402c4bb457ab
Mihail1992x2 <119675734+Mihail1992x2@users.noreply.github.com>
2023-02-09 10:05:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 #Configure the AWS Provider
 provider "aws" {
   region     = "eu-west-1"
-#  shared_credentials_files = ["credentials"]
-# [default]
+  shared_credentials_files = ["credentials"]
+  [default]
 
 }
 
